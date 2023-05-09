ee7dba496c4aa48a647a38a554a4eb3cb99bd17a
RAHUL KUMAR <rahul162rk@gmail.com>
2023-05-09 09:06:48 +0530

    Create day-3-createCounterII.js

---
+++ day-3-createCounterII.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/counter-ii/
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+  let tmp = init;
+  return {
+    increment: () => ++tmp,
+    decrement: () => --tmp,
+    reset: () => tmp = init
+  };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
