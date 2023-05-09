13366cfbe526943aa3aa6079bfe85b7f1a84e4bb
RAHUL KUMAR <rahul162rk@gmail.com>
2023-05-09 09:08:41 +0530

    Create day-5-filter.js

---
+++ day-5-filter.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/filter-elements-from-array/
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let temp = [];
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i))
+            temp.push(arr[i]);
+    }
+    return temp;
+};
